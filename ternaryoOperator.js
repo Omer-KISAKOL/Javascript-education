8778dc50c663a389d2fe4d7c368fc095880e400c
ÖMER KISAKOL <okisakol@gmail.com>
2023-10-26 08:15:24 +0300

    ternaryo

---
+++ ternaryoOperator.js
@@ -0,0 +1,5 @@
+const username = "okisakol";
+const password = "1234567";
+
+password.length > 6 ? console.log("yes") : console.log("no");
+username.length > 12 ? console.log("yes") : console.log("no");
