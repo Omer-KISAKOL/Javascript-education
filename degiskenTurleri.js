ea16756f3fb146c7405f7ebed47481b96be7285d
ÖMER KISAKOL <okisakol@gmail.com>
2023-10-26 08:02:06 +0300

    değişkenler

---
+++ degiskenTurleri.js
@@ -0,0 +1,17 @@
+let s1 = "1";
+let s2 = "2";
+let top = Number(s1) + Number(s2);
+// let top = s1.toString() + s2.toString();
+console.log(typeof top);
+
+
+console.log(typeof false);
+console.log(typeof true);
+let s3 = 10;
+let s4 = 20;
+console.log(s3 < s4);
+console.log(s3 == s4);
+
+
+let undefined;
+console.log(typeof undefined);
