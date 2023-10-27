e50895c2f2fd79b21b068629b0c666e85846cd70
ÖMER KISAKOL <okisakol@gmail.com>
2023-10-27 07:43:40 +0300

    object

---
+++ object.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName : "Ömer",
+    lastName : "KISAKOL",
+    id : 4121,
+    age : 23,
+    eyeColor : "green"
+};
+
+
+console.log(person.id + ": " + person.firstName + " is " + person.age + " years old.");
