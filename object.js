b1560cd6a2ad12745c92da142a8d7f9ebed2f60a
ÖMER KISAKOL <okisakol@gmail.com>
2023-10-31 00:36:06 +0300

    objects

---
+++ object.js
@@ -0,0 +1,19 @@
+let urunler =[
+    {
+        "id": 1,
+        "urunAdi": "huawei",
+        "fiyati": 10
+    },
+    {
+        "id": 2,
+        "urunAdi": "apple",
+        "fiyati": 20
+    },
+    {
+        "id": 3,
+        "urunAdi": "vestel",
+        "fiyati": 30
+    },
+]
+
+console.log(urunler);
